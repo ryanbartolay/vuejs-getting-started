69afbe19f1504abc26e469ef4529d2cbabd64418
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 15:00:37 +0800

    Vue JS 2 Tutorial #13 - Simple Punchbag Game

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 new Vue({
   el: '#vue-app',
   data: {
-    error: false,
-    success: false
+    health: 100,
+    ended: false
+  },
+  methods: {
+    punch: function() {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    restart: function() {
+      this.health = 100;
+      this.ended = false;
+    }
   },
   computed: {
-  
+
   }
 });
