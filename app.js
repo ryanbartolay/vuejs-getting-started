56455f72c78200612da8aeb11283613169848001
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 10:40:56 +0800

    Vue JS 2 Tutorial #5 - Events with canvas for getting x and y position

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ new Vue({
     job: "Software Engineer",
     website: "http://ryan.com",
     age: 33,
-    websiteHTML: '<a href="http://ryan.com">Home</a>'
+    websiteHTML: '<a href="http://ryan.com">Home</a>',
+    x: 0,
+    y: 0,
   },
   methods: {
     greet: function(time) {
       return "Good " + time + " " + this.name
     },
     add: function(value) {
-      this.age = this.age + value
+      this.age += value
     },
     subtract: function(value) {
-      this.age = this.age - value
+      this.age -= value
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
     }
   }
 });
