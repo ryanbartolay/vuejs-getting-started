126db7b978cf2d36b8131d172105f5207e56928d
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 09:49:52 +0800

    Vue JS 2 Tutorial #4 - Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: "Ryan",
-    job: "Software Engineer"
+    job: "Software Engineer",
+    website: "http://ryan.com",
+    websiteHTML: '<a href="http://ryan.com">Home</a>'
   },
   methods: {
     greet: function(time) {
