821dd39c152abfee8995fb5c1445f239eef5bb30
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 11:02:44 +0800

    Vue JS 2 Tutorial #8 - Two-Way Data Binding

--- app.js
+++ app.js
@@ -2,29 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: "Ryan",
-    job: "Software Engineer",
-    website: "http://ryan.com",
     age: 33,
-    websiteHTML: '<a href="http://ryan.com">Home</a>',
-    x: 0,
-    y: 0,
   },
   methods: {
-    greet: function(time) {
-      return "Good " + time + " " + this.name
-    },
-    add: function(value) {
-      this.age += value
-    },
-    subtract: function(value) {
-      this.age -= value
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX
-      this.y = event.offsetY
-    },
-    click: function() {
-      alert("button clicked")
-    }
+    
   }
 });
