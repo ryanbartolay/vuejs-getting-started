f044cd7ce5e69f60e0a218e8c3e4dbed02538bdc
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 20:34:12 +0800

    Vue JS 2 Tutorial #16 - Refs

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
-Vue.component('greeting', {
-  template: `
-  <span>Hello World {{name}}
-  <button v-on:click="changeName()">Change Name</button>
-  </span>
-  `,
-  data: function() {
-    return {
-      name: "Ryan"
-    }
-  },
-  methods: {
-    changeName: function() {
-      this.name = "Miguel";
-    }
-  }
-});
-
-var one = new Vue({
-  el: '#vue-app-one',
+new Vue({
+  el: '#vue-app',
   data: {
-    title: "Vue App One"
+    output: "Pizza"
   },
   methods: {
-
-  },
-  computed: {
-
-  }
-});
-
-var two = new Vue({
-  el: '#vue-app-two',
-  data: {
-    title: "Vue App Two"
-  },
-  methods: {
-
+    submitFood: function() {
+      this.output = this.$refs.input.value;
+    }
   },
   computed: {
 
