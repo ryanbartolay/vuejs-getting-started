dde254402738b33997cdbc945b0820eab729c3d2
Ryan Kristoffer Bartolay <ryank@peplink.com>
2018-09-15 11:59:24 +0800

    Vue JS 2 Tutorial #10 - Dynamic CSS Classes first example

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 new Vue({
   el: '#vue-app',
   data: {
-    age: 33,
-    a: 0,
-    b: 0
-  },
-  computed: {
-    addToA: function() {
-      console.log("running a")
-      return this.age + this.a
-    },
-    addToB: function() {
-      console.log("running b")
-      return this.age + this.b
-    }
+    available: true
   }
+
 });
